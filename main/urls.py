7cde323493e6487e583b0e87e691043682985aa7
jerrytigerxu <jere.tiger.xu@gmailc.om>
2022-09-13 11:17:48 -0400

    adjustments

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-"""djPortfolio URL Configuration
+"""'main' URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
-    path("blog/", include("blog.urls")),
 ]
